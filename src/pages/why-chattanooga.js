082a36eeb7996d1983d24b213e6bb8fcad71f4c7
Brett Wise <brettdwise@gmail.com>
2018-05-02 14:08:37 -0400

    Added link in menu and page stub

---
+++ src/pages/why-chattanooga.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "gatsby-link";
+import SectionWrapper from "../components/SectionWrapper";
+
+const WhyChattanooga = () => (
+  <SectionWrapper
+    header="Why Chattanooga?"
+    subhead="Let us count the ways."
+    suppressLine={true}
+  >
+    
+  </SectionWrapper>
+);
+
+export default WhyChattanooga;
