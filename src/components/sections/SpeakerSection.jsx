49ccc29ead0c07eaae97aa9825d62379c5229041
Brett Wise <brettdwise@gmail.com>
2018-05-03 09:50:53 -0400

    Added Chris K bio

--- src/components/sections/SpeakerSection.jsx
+++ src/components/sections/SpeakerSection.jsx
@@ -107,6 +107,17 @@ const ChrisBio = (
   </p>
 );
 
+const ChrisKBio = (
+  <p>
+    Chris is a software engineer building services and applications for Bleacher
+    Report. Although he started out writing C for embedded systems, these days
+    he spends his time in Elixir, Haskell, Go, and Rust. When not writing code
+    for work, Chris can be found writing code for fun, talking about the joys of
+    functional programming, playing pinball, roasting coffee, or building lego
+    with his kids.
+  </p>
+);
+
 const StuBio = (
   <div>
     <p>
@@ -274,6 +285,7 @@ const SpeakerSection = () => (
       <Bio
         name="Chris Keathley"
         bioName="Chris'"
+        bio={ChrisKBio}
         twitterLink="https://twitter.com/ChrisKeathley"
         twitterHandle="@ChrisKeathley"
         pic={chriskPic}
