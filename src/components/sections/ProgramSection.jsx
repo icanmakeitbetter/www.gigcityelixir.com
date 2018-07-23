3aaba07600b59e5b79fa87a7b3c18c1aecc54c6a
Brett Wise <brettdwise@gmail.com>
2018-07-23 15:44:15 -0400

    Formatting and added in actual link to page

--- src/components/sections/ProgramSection.jsx
+++ src/components/sections/ProgramSection.jsx
@@ -6,32 +6,33 @@ import Link from "gatsby-link";
 const ProgramSection = () => (
   <SectionWrapper
     header="Program"
-    subhead="A little more on conference programming."
-  >
+    subhead="A little more on conference programming.">
     <p>
-    The software industry has about as many libraries and projects to help you
-    get things done as it does programmers. Lots of conferences focus on those.
-    That’s a fine approach. GigCityElixir doesn’t. 
+      The software industry has about as many libraries and projects to help you
+      get things done as it does programmers. Lots of conferences focus on
+      those. That’s a fine approach. GigCityElixir doesn’t.
     </p>
-    
+
     <p>
-    In a world making a slow transition between object oriented and functional paradigms, our conference
-    is packed with experienced developers and industry leaders who have been
-    active voices for both. They are there to do one thing: make you a better
-    programmer by focusing on the foundational skills you’ll take with you no
-    matter the tech stack. 
+      In a world making a slow transition between object oriented and functional
+      paradigms, our conference is packed with experienced developers and
+      industry leaders who have been active voices for both. They are there to
+      do one thing: make you a better programmer by focusing on the foundational
+      skills you’ll take with you no matter the tech stack.
     </p>
-    
+
     <p>
-    So whether seasoned software engineer or Johnny-come-lately there’s a lot of something for everyone. 
+      So whether seasoned software engineer or Johnny-come-lately there’s a lot
+      of something for everyone.
     </p>
-    
+
     <p>
-    New ideas and new friends await. Look forward to seeing you all in October!
+      New ideas and new friends await. Look forward to seeing you all in
+      October!
     </p>
-    {/* <Link to=""> */}
+    <Link to="/program/">
       <Button className="button" text="Program In Depth" />
-    {/* </Link> */}
+    </Link>
   </SectionWrapper>
 );
 
