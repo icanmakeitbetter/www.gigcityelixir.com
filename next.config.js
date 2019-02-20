2207769fac1464be54edf06648e18a9623350fbb
Colby Blaisdell <ccblaisdell@gmail.com>
2019-02-19 22:04:54 -0600

    Switch from npm to yarn bc bug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,2 @@
 const withFonts = require("next-fonts");
 module.exports = withFonts();
-
-defaultLoaders.babel.options.plugins.push(["linaria/babel"]);
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,16 @@
-import React from "react";
-import get from "lodash/get";
-
-import SectionWrapper from "../components/SectionWrapper";
-import JoseSection from "../components/sections/JoseSection";
-import AboutUsSection from "../components/sections/AboutUsSection";
-import ProgramSection from "../components/sections/ProgramSection";
-import SpeakerSection from "../components/sections/SpeakerSection";
-import SubscriptionSection from "../components/sections/SubscriptionSection";
-import ScholarshipSection from "../components/sections/ScholarshipSection";
-import TicketSection from "../components/sections/TicketSection";
-import ScheduleSection from "../components/sections/ScheduleSection";
-import HotelSection from "../components/sections/HotelSection";
-import SponsorSection from "../components/sections/SponsorSection";
-
-class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, "props.data.site.siteMetadata.title");
-
-    return (
-      <div>
-        <JoseSection />
-        <AboutUsSection />
-        <SpeakerSection />
-        <ScheduleSection />
-        <ProgramSection />
-        <SponsorSection />
-        <SubscriptionSection />
-        <ScholarshipSection />
-        <TicketSection />
-        <HotelSection />
-      </div>
-    );
-  }
+export default function Index() {
+  return (
+    <div>
+      <Header>
+        <Logo />
+        <DateAndVenue />
+        <About />
+        <NewsLetterSignup />
+      </Header>
+      <ProgramSummary />
+      <Sponsors />
+      <Accomodations />
+      <Footer />
+    </div>
+  );
 }
-
-export default BlogIndex;
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
