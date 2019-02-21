fd92aa3ff684008cfeba422eedb9d534a776d586
Colby Blaisdell <ccblaisdell@gmail.com>
2019-02-20 23:01:22 -0600

    Tweak button styles so links dont have to wrap buttons

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { colors } from "../utils/constants";
 import { css } from "@emotion/core";
+import { typography } from "../utils/typography";
 
-const Button = ({ text, alert }) => (
-  <button
-    css={css`
-      background-color: ${alert ? colors.peach : colors.plum};
-      border-color: ${alert ? colors.peach : colors.plum};
-      border-radius: 3px;
-      border-style: solid;
-      color: white;
-      cursor: pointer;
-      font-family: "OdudoMono-Regular", sans-serif;
-      font-size: 32px;
-      height: 78px;
-      width: 100%;
-    `}
-  >
-    {text}
-  </button>
-);
+export const buttonCss = css`
+  background-color: ${colors.plum};
+  border: 0;
+  border-radius: 3px;
+  color: white;
+  cursor: pointer;
+  font-family: "OdudoMono-Regular", sans-serif;
+  font-size: ${typography.rhythm(1.25)};
+  padding: ${typography.rhythm(3 / 4)} 0;
+  width: 100%;
+  display: block;
+  text-align: center;
+  text-decoration: none;
+  :hover,
+  :focus {
+    background-color: ${colors.peachy_plum};
+  }
+  :active {
+    background-color: ${colors.dusk};
+  }
+`;
+
+const Button = ({ text }) => <button css={buttonCss}>{text}</button>;
 
 export default Button;
