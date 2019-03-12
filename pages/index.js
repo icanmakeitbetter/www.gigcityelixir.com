765506c6c7cd1d31c2f4141f4cdf3dc2e6c2909a
Colby Blaisdell <ccblaisdell@gmail.com>
2019-03-12 11:25:39 -0500

    Add noop service worker

--- pages/index.js
+++ pages/index.js
@@ -20,11 +20,18 @@ import GCEHead from "../components/GCEHead";
 
 export default function Index() {
   useEffect(() => {
-    navigator.serviceWorker.getRegistrations().then(function(registrations) {
-      for (let registration of registrations) {
-        registration.unregister();
-      }
-    });
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/static/sw.js", { scope: "/static/" })
+        .then(function(reg) {
+          // registration worked
+          console.log("Registration succeeded. Scope is " + reg.scope);
+        })
+        .catch(function(error) {
+          // registration failed
+          console.log("Registration failed with " + error);
+        });
+    }
   }, []);
 
   return (
